e70b25d1a168293fab3deac1a542a78cf9e8a747
Savepong <savepong.com@gmail.com>
2021-03-21 21:29:54 +0700

    Create separate an brewery component

---
+++ brewery-detail.js
@@ -0,0 +1,19 @@
+import { LitElement, html } from "https://unpkg.com/lit-element?module";
+
+class BreweryDetail extends LitElement {
+  static get properties() {
+    return {
+      name: { type: String },
+      type: { type: String },
+      city: { type: String },
+    };
+  }
+
+  render() {
+    return html`
+      <h3>${this.name}</h3>
+      <p>brewery type: ${this.type}</p>
+      <p>city: ${this.city}</p>
+    `;
+  }
+}
